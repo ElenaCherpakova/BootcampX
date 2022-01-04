7af8b39eec2c029662c2f51c9f20576ddc37744e
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-04 04:13:23 +0000

    Get the total number of assignments for each day, busy_days, Get all cohorts with 18 or more students, Get the total number of assignment submissions for each cohort.Get currently enrolled students' average assignment completion time.Get the students who's average time it takes to complete an assignment is less than the average estimated time

---
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort, count (students.name) AS total_submissions
+FROM cohorts 
+JOIN students ON cohorts.id=cohort_id
+JOIN assignment_submissions ON students.id=student_id
+GROUP BY cohorts.name
+ORDER BY count (students.name) DESC;
+
