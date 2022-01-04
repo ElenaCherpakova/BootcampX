a18ca11dd7168ff9c18cbf38095191eb2f5542a1
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-04 19:20:10 +0000

    do queries for 1_total_teacher_assistance_requests, 2_total_students_assistance_requests, 3_assistance_request_data

--- 4_queries/1_total_teacher_assistance_requests.sql
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -1,9 +1,8 @@
-SELECT teachers.name as name, count(*) AS total_assistance
-FROM teachers
-JOIN assistance_requests
-ON teachers.id=teacher_id
+SELECT teachers.name as name, count(assistance_requests.*) as total_assistances
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
 WHERE name = 'Waylon Boehm'
-GROUP BY name;
+GROUP BY teachers.name;
 
 
 
