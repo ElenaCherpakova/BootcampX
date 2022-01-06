b5af80fdf54ba74b367a37ae5305dbaee55e08d9
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-06 02:40:15 +0000

    parameterize query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
-const cohortName = process.argv[2]
+const cohortName = process.argv[2] || 'JUL02'
+const values = [`%${cohortName}%`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,18 +9,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohortName || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;`
-)
+
+pool.query(queryString, values)
   .then(res => {
-    res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`);
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
     })
   }).catch(err => console.error('query error', err.stack));
 
